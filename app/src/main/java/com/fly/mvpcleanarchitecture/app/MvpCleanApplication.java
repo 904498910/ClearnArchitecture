a1ceedff1ea388f790aea98de381c95ea1f550a2
baofly <904498910@qq.com>
2016-08-24 09:53:25 +0800

    add Logger

--- app/src/main/java/com/fly/mvpcleanarchitecture/app/MvpCleanApplication.java
+++ app/src/main/java/com/fly/mvpcleanarchitecture/app/MvpCleanApplication.java
@@ -3,7 +3,6 @@ package com.fly.mvpcleanarchitecture.app;
 import android.app.Application;
 
 import com.fly.mvpcleanarchitecture.BuildConfig;
-import com.fly.mvpcleanarchitecture.app.config.Constants;
 import com.fly.mvpcleanarchitecture.app.dependencyinjection.ApiServiceModule;
 import com.fly.mvpcleanarchitecture.app.dependencyinjection.ApplicationComponent;
 import com.fly.mvpcleanarchitecture.app.dependencyinjection.ApplicationModule;
@@ -18,18 +17,17 @@ import com.orhanobut.logger.Logger;
 public class MvpCleanApplication extends Application {
 
     private static ApplicationComponent applicationComponent;
-    private final String LOG_TAG = "MvpCleanApplication";
 
     @Override
     public void onCreate() {
         super.onCreate();
         applicationComponent = DaggerApplicationComponent.builder().applicationModule(new
                 ApplicationModule(this)).apiServiceModule(new
-                ApiServiceModule(Constants.ServerUrl)).build();
+                ApiServiceModule(BuildConfig.ServerUrl)).build();
         if (BuildConfig.IS_DEBUG) {
-            Logger.init(LOG_TAG).logLevel(LogLevel.FULL);
+            Logger.init(BuildConfig.LOG_TAG).logLevel(LogLevel.FULL);
         } else {
-            Logger.init(LOG_TAG).logLevel(LogLevel.NONE);
+            Logger.init(BuildConfig.LOG_TAG).logLevel(LogLevel.NONE);
         }
     }
 
